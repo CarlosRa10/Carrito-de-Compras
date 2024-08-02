d6a5b53b876ccc43a9a1ad0d847b96844e53ebed
Carlosra10 <carloseduardoramirez10@gmail.com>
2024-08-02 17:08:29 -0400

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -21,28 +21,62 @@
 //useEffect se ejecuta automaticamente cuando el componente esta listo, es un buen lugar para colocar código para consultar una API o LocalStorage
 //Debido a que le podemos pasar una dependencia y estar escuchando por los cambios que sucedan en una variable, puede actualizar el componente cuando ese cambio suceda
 //Dependiendo del valor que pasemos en el array de dependencias (o no pasemos nada) el hook de udeEffect hará algo diferente.
+//Statements - Una app de JS es una serie de statements, cada statement es una instrucción para hacer algo 
+//Algunos statements son:
+//Creacion de variables- Codigos de condicionales con if - Lanzar errores con throw new Error() - Iterar con While o for
+//Expressions - Una expresión es algo que produce un valor.
+//Algunas Expressions son:
+//Ternarios - Utilizar un Array Method que genera un nuevo Array - .map que genera un nuevo array a diferencia de forEach
+//Props en React - Es una forma de compartir informacón entre componentes
+//Los componentes de React utilizan Props para comunicarse entre ellos. Puedes pasarle información de un componente padre al hijo por medio de estos props.
+//Los props se parecen a los atributos en HTML, pero puedes pasarle arrays, objetos o funciones
+//Los Props se pasan del padre al hijo, nunca se pueden pasar del hijo al padre
+//Eventos en React - la forma en que React maneja los eventos es muy similar a como lo hace JavaScript de forma nativa con algunos cambios.
+//Los eventos son camelCase, es decir en lugar de onchange se utiliza onChange, en lugar de onclick se utiliza onClick
+//Tambien a diferencia de JS y HTML, donde se coloca el nombre de la función en un string en React (JSX) se utiliza la función entre llaves {} 
 
 
 import { useState, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
+import { db } from "./data/db"
 //Puedes importar los componentes o ponerlos directamente en el codigo y te los importa
 
 function App() {
 
-  //State
-  const [auth, setAuth] = useState(false)
-  // console.log(auth)
+  const [data, setData] = useState(db)//Si lo haces con una api, inicias con un arreglo vacio ([]) y consultas tu api y se llena tu state
+  //si fuera una api, sin dependencias esta seria la recomendada
+  //const [data, setData] = useState([])
+  // useEffect(()=>{
+  //   setData(db)
+  // },[])
 
-  useEffect(()=>{
-    if(auth){
-      console.log('Autenticado')
+
+  // //State
+  // const [auth, setAuth] = useState(false)
+  // // console.log(auth)
+
+  // useEffect(()=>{
+  //   if(auth){
+  //     console.log('Autenticado')
+  //   }
+  // },[auth])
+
+  // setTimeout(() => {
+  //   setAuth(true)
+  // }, 3000);
+  const [cart, setCart] = useState([])
+
+
+  function addToCart(item){
+    const itemExists = cart.findIndex(guitar=>guitar.id === item.id)
+    if(itemExists >=0) {//El elemento existe en el carrito
+      console.log('Ya existe...')
+    }else{
+      item.quantity = 1
+      setCart([...cart,item])
     }
-  },[auth])
-
-  setTimeout(() => {
-    setAuth(true)
-  }, 3000);
+  }
 
 
   return (//El return es lo que se muestra en pantalla
@@ -54,12 +88,14 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
-            <Guitar/>
+          {data.map((guitar)=>(
+            <Guitar
+            key={guitar.id}
+                guitar={guitar}
+                setCart={setCart}
+                addToCart={addToCart}
+            />
+          ))}
         </div>
     </main>
 
